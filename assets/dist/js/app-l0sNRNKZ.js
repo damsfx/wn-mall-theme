43f1a3cc636c34b90ed5c4bc9753af2fa22f6f0d
Damien MATHIEU (DamsFX) <damsfx@gmail.com>
2026-05-18 18:42:35 +0200

    WIP polishing

--- assets/dist/js/app-l0sNRNKZ.js
+++ assets/dist/js/app-l0sNRNKZ.js
@@ -1,5 +1,5 @@
 /**!
- * wn-mall-theme - 2.0.0 - 20260516-183213
+ * wn-mall-theme - 2.0.0 - 20260518-184210
  * Official starter theme for the Winter.Mall e-commerce plugin
  *
  * Tobias Kuendig - OFFLINE (https://www.offline.swiss/)
